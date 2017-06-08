76ed641498babcebb6ccf239a632c5ae1f7478e0
radeita <ortigas.ronel@gmail.com>
2017-06-08 20:18:38 +0800

    Change sockets

--- Noborderv2.0_/public/js/cons.js
+++ Noborderv2.0_/public/js/cons.js
@@ -1,2 +1,3 @@
 var SOCKETPORT = "http://noborderbclubnode.herokuapp.com";
-var LOCALPORT = "http://localhost:5000";
+var LOCALPORT = "http://noborderbclubnode.herokuapp.com";
+//var LOCALPORT = "http://localhost:5000";
