2f39aae3bd8f37d1dbb89262ce370654b6b833f5
Icontent Doe <icontentdoe@gmail.com>
2017-06-12 10:36:02 +0800

    fix

--- Noborderv2.0_/resources/views/email/welcome/index.blade.php
+++ Noborderv2.0_/resources/views/email/welcome/index.blade.php
@@ -7,5 +7,3 @@ Please verify Your Email Address by clicking the link below.<br><br>
 <a href='<?php echo "http://noborderclub.herokuapp.com/validate_email/$user->verification/$user->email" ?>'>
    <?php echo "http://noborderclub.herokuapp.com/validate_email/$user->verification/$user->email" ?>
 </a>
-
-asd
