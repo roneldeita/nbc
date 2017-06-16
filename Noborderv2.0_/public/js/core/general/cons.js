d418e391ab143704119ce8cd700d228c88ac5025
radeita <ortigas.ronel@gmail.com>
2017-06-16 15:51:23 +0800

    qwe

--- Noborderv2.0_/public/js/core/general/cons.js
+++ Noborderv2.0_/public/js/core/general/cons.js
@@ -5,7 +5,7 @@ var LOCAL_PORT = "http://localhost:5000";
 var REMOTE_URL = "http://noborderclub.herokuapp.com";
 var LOCAL_URL = "http://localhost:8000";
 
-var CURRENT_PORT = LOCAL_PORT;
-var CURRENT_URL = LOCAL_URL;
+var CURRENT_PORT = REMOTE_PORT;
+var CURRENT_URL = REMOTE_URL;
 
 var TIME_INTERVAL = 2000;
